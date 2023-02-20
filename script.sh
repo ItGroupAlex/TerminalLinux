868aa88d326f07192cbaf2c458d4cf81d248e9b8
Alex <125738088+ItGroupAlex@users.noreply.github.com>
2023-02-20 21:46:20 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,10 @@
+mkdir foldername
+cd foldername
+mkdir folder1 folder2 folder3
+cd folder1
+touch file1.txt file2.txt file3.txt jsonfile1.json jsonfile2.json
+mkdir folder4 folder5 folder6
+ls -l
+cd ..
+mv folder1/file1.txt folder2/file1.txt
+mv folder1/file2.txt folder2/file2.txt
